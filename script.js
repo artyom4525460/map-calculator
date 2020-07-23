e5f6748a645407b74f405aef96085cca34518276
Artyom Zhukov <artyom.zhukov@progforce.com>
2020-07-23 17:27:58 +0300

    WIP - scripts for slider block

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
+
 var map, marker, circle;
 var myLatLng = {lat: 43.651499, lng: -79.383466};
-var image = './images/baloon.png';
+
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -11,7 +13,7 @@ function initMap() {
   marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    icon: image
+    icon: './images/baloon.png'
   });
   circle = new google.maps.Circle({
     strokeWeight: 0,
@@ -19,50 +21,92 @@ function initMap() {
     fillOpacity: 0.35,
     map: map,
     center: myLatLng,
-    radius: 1000
+    radius: 1000 * slider.size.value
   });
 }
 
-/*var citymap = {
-    chicago: {
-      center: { lat: 41.878, lng: -87.629 },
-      population: 2714856
-    },
-    newyork: {
-      center: { lat: 40.714, lng: -74.005 },
-      population: 8405837
-    },
-    losangeles: {
-      center: { lat: 34.052, lng: -118.243 },
-      population: 3857799
-    },
-    vancouver: {
-      center: { lat: 49.25, lng: -123.1 },
-      population: 603502
-    }
-  };
+var mapObj = {
+  updateCircle: function(){
+    circle.setRadius(1000 * slider.size.value);
+  },
+  updateScale: function(){
+
+  }
+}
+
+var element = {
+  minus           : document.getElementById("minus"),
+  plus            : document.getElementById("plus"),
+  radiusValue     : document.getElementById("radius-value"),
+  calculatedCost  : document.getElementById("calculated-cost"),
   
-  function initMap() {
-    // Create the map.
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: { lat: 37.09, lng: -95.712 },
-      mapTypeId: "terrain"
-    });
-  
-    // Construct the circle for each value in citymap.
-    // Note: We scale the area of the circle based on the population.
-    for (var city in citymap) {
-      // Add the circle for this city to the map.
-      var cityCircle = new google.maps.Circle({
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.35,
-        map: map,
-        center: citymap[city].center,
-        radius: Math.sqrt(citymap[city].population) * 100
-      });
+  disable: function(element){
+    element.classList.add('block-element');
+  },
+  enable: function(element){
+    element.classList.remove('block-element');
+  },
+  initElements: function(){
+    this.radiusValue.innerHTML = slider.size.value;
+    if(slider.size.value == 1){
+      element.disable(element.minus);
     }
-  }*/
+    if(slider.size.value == 100){
+      element.disable(element.plus);
+    }
+  }
+}
+
+var slider = {
+  size: document.getElementById("radius-slider"),
+  minSize: 1,
+  maxSize: 100,
+
+  increaseValue: function(){
+    if (this.size.value <= this.maxSize){
+      this.size.value++;
+      this.updateValue();
+      element.enable(element.minus);
+    }
+    else{
+      element.disable(element.plus);
+    }
+  },
+
+  reduceValue: function(){
+    console.log('reduce');
+    if(this.size.value >= this.minSize){
+      this.size.value--;
+      this.updateValue();
+      element.enable(element.plus);
+    }
+    else{
+      element.disable(element.minus);
+    }
+  },
+
+  updateValue: function(){
+    mapObj.updateCircle();
+    element.radiusValue.innerHTML = this.size.value;
+  },
+
+  moveButton: function(){
+    this.updateValue();
+    switch(this.size.value){
+      case '100': 
+        element.enable(element.minus);
+        element.disable(element.plus);
+        break;
+      case '1': 
+        element.disable(element.minus);
+        element.enable(element.plus);
+        break;
+      default:
+        element.enable(element.minus);
+        element.enable(element.plus);
+    }
+  }
+};
+
+
+element.initElements();
